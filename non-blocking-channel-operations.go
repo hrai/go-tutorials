f5f73ad1b48f4d9dc70b22ee70f2a591befa34fd
Hangjit Rai <hangjit.rai@outlook.com>
2020-07-05 10:16:28 +1000

    Added Non-Blocking-Channel-Operations.go

--- non-blocking-channel-operations.go
+++ non-blocking-channel-operations.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    messages := make(chan string, 1)
-    signals := make(chan bool, 1)
+    messages := make(chan string)
+    signals := make(chan bool)
 
     select {
     case msg := <-messages:
