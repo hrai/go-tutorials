47e41df8e60885571877f9a069d90b55ca51ed08
Hangjit Rai <hangjit.rai@outlook.com>
2020-07-05 15:22:11 +1000

    Added Sort.go

--- sort.go
+++ sort.go
@@ -9,5 +9,12 @@ func main() {
     strs:=[]string{"c","b","a"}
     sort.Strings(strs)
     fmt.Println(strs)
+
+    ints:=[]int{32,2,29}
+    sort.Ints(ints)
+    fmt.Println(ints)
+
+    s:=sort.IntsAreSorted(ints)
+    fmt.Println("Sorted:",s)
 }
 
