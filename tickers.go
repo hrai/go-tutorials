7108a16c5252512e2156e743861254831ca401e5
Hangjit Rai <hangjit.rai@outlook.com>
2020-07-05 10:37:50 +1000

    Added Tickers.go

---
+++ tickers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+
+    ticker := time.NewTicker(500 * time.Millisecond)
+    // done := make(chan bool)
+
+    // for elem:= range ticker.C{
+    //     fmt.Println(elem)
+    // }
+
+    go func() {
+        for {
+            select {
+            // case <-done:
+            //     return
+            case t := <-ticker.C:
+                fmt.Println("Tick at", t)
+            }
+        }
+    }()
+
+    time.Sleep(1600 * time.Millisecond)
+    ticker.Stop()
+    // done <- true
+    fmt.Println("Ticker stopped")
+}
